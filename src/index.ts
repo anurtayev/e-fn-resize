65dcc730e67b1ae50c44ac689a58012ca01e1bbb
Adil Nurtayev <anurtayev@torstar.ca>
2022-11-13 10:13:08 -0500

    feat: finished resizer

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,15 @@ import { APIGatewayProxyEventV2, APIGatewayProxyHandlerV2 } from "aws-lambda";
 import S3 from "aws-sdk/clients/s3";
 import convert from "heic-convert";
 import sharp from "sharp";
+import { array, mixed, object, string, ValidationError, number } from "yup";
+
+const { log } = console;
+
+const validationSchema = object({
+  key: string().trim().lowercase().required(),
+  width: number().required(),
+  height: number().required(),
+});
 
 const s3 = new S3({
   apiVersion: "2006-03-01",
@@ -14,23 +23,20 @@ assert(mediaBucket, "Bucket name environment variable is required");
 const resizerBucket = process.env.RESIZER_BUCKET;
 assert(resizerBucket, "Bucket name environment variable is required");
 
-const getParams = (event: APIGatewayProxyEventV2) => {
-  const { queryStringParameters } = event;
-  const { width, height, key } = queryStringParameters;
-  assert(
-    width && height && key,
-    "Height and width query string parameters are required"
-  );
-
-  return { key, width: Number(width), height: Number(height) };
+type InputEvent = {
+  key: string;
+  width: number;
+  height: number;
 };
 
-export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
-  console.log({ event, context });
+export const handler = async (event: InputEvent, context) => {
+  log({ event, context });
 
-  const { key, width, height } = getParams(event);
+  if (!(await validationSchema.isValid(event))) {
+    throw new Error("invalid input");
+  }
 
-  console.log({ key, width, height });
+  const { key, width, height } = event;
 
   const { Body: imgBuffer } = await s3
     .getObject({
@@ -39,19 +45,31 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
     })
     .promise();
 
-  const outputBuffer = await convert({
+  log("downloaded file");
+
+  const convertedBuffer = await convert({
     buffer: imgBuffer as Buffer,
     format: "JPEG",
     quality: 1,
   });
 
+  log("converted file to JPEG");
+
+  const resizedBuffer = await sharp(convertedBuffer as Buffer)
+    .resize(width, height)
+    .toBuffer();
+
+  await s3
+    .putObject({
+      Bucket: resizerBucket,
+      Key: `${key}/${width}x${height}.jpeg`,
+      Body: resizedBuffer,
+    })
+    .promise();
+
+  console.log("uploaded file");
+
   return {
     statusCode: 200,
-    isBase64Encoded: true,
-    body: (
-      await sharp(imgBuffer as Buffer)
-        .resize(width, height)
-        .toBuffer()
-    ).toString("base64"),
   };
 };
